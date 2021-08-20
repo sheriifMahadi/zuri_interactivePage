7fdddbd982e7e59a71775431df669aae919f5b19
sheriifMahadi <sheriffabdulamjid@gmail.com>
2021-08-20 06:13:01 +0000

    Update ALLOWED_HOSTS with site and development server URL

--- interactive_resume/settings.py
+++ interactive_resume/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-z1=n(13d=jcsmh
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ancient-garden-21230.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
